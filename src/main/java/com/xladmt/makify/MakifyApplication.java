b7d1f7c2e87044bc03529aa8bb25393d407793c5
HiSeungmin <osminosmin7180@gmail.com>
2025-02-14 18:36:12 +0900

    initialized

---
+++ src/main/java/com/xladmt/makify/MakifyApplication.java
@@ -0,0 +1,13 @@
+package com.xladmt.makify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xladmt/makify/MakifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xladmt.makify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
