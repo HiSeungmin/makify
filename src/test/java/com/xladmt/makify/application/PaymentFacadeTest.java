0ddeeb943c5ed9ee809b1dc0754470e21b036fc9
HiSeungmin <osminosmin7180@gmail.com>
2025-09-06 15:40:45 +0900

    env 수정

---
+++ src/test/java/com/xladmt/makify/application/PaymentFacadeTest.java
@@ -0,0 +1,13 @@
+package com.xladmt.makify.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("PaymentFacade 테스트")
+class PaymentFacadeTest {
+
+}
