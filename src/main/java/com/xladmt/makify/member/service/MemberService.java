93db8319c45899c42267e7b0f49c633d0d8a9e80
HiSeungmin <osminosmin7180@gmail.com>
2026-02-03 22:04:25 +0900

    챌린지 생성 UI 추가

--- src/main/java/com/xladmt/makify/member/service/MemberService.java
+++ src/main/java/com/xladmt/makify/member/service/MemberService.java
@@ -7,13 +7,10 @@ import com.xladmt.makify.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
