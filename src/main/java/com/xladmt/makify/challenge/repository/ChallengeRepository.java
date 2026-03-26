460f94158fa80c8c7051725d0ac86987966a363a
HiSeungmin <osminosmin7180@gmail.com>
2026-03-26 13:54:16 +0900

    인기 챌린지 수정

--- src/main/java/com/xladmt/makify/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/xladmt/makify/challenge/repository/ChallengeRepository.java
@@ -3,11 +3,11 @@ package com.xladmt.makify.challenge.repository;
 import com.xladmt.makify.common.constant.Category;
 import com.xladmt.makify.common.constant.YN;
 import com.xladmt.makify.common.entity.Challenge;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,10 +15,10 @@ import java.util.Optional;
 
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-    
+
     @Query("SELECT c FROM Challenge c JOIN FETCH c.member WHERE c.id = :id")
     Optional<Challenge> findByIdWithMember(@Param("id") Long id);
-    
+
     /**
      * 챌린지 검색 (제목, 설명 키워드 + 카테고리 필터)
      */
@@ -35,4 +35,7 @@ public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
     );
 
     List<Challenge> findAllByIsVisibleOrderByCreatedAtDesc(YN isVisible);
+
+    @Query("SELECT c FROM Challenge c WHERE c.id IN :ids AND c.isVisible = 'Y'")
+            List<Challenge> findHomeChallenge(@Param("ids") List<Long> ids);
 }
--- src/main/java/com/xladmt/makify/home/controller/HomeController.java
+++ src/main/java/com/xladmt/makify/home/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.xladmt.makify.home.controller;
 
+import com.xladmt.makify.challenge.repository.ChallengeRepository;
+import com.xladmt.makify.common.entity.Challenge;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final ChallengeRepository challengeRepository;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model) {
+        List<Challenge> homeChallenges = challengeRepository.findHomeChallenge(List.of(1L, 3L, 4L));
+        model.addAttribute("homeChallenges", homeChallenges);
         return "home";
     }
 
     @GetMapping("/notifications")
-    public String notifications(){ return "notifications"; }
+    public String notifications() {
+        return "notifications";
+    }
 }
